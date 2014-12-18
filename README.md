d0af994f53bec1e1ff54d5a871cfbce0120c57c4
Félix Saparelli <felix.saparelli@mckay.co.nz>
2014-12-19 12:34:46 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# slk
+
+_Slack wrapper for use as desktop app._
+
+![Screenshot](https://stuff.passcod.name/humgur/code/slk.png)
+
+Uses [node-webkit] to provide a single-purpose window
+onto Slack's web version.
+
+Will not work with node-webkit < 0.10.
+
+[node-webkit]: https://github.com/rogerwang/node-webkit
+[release]: https://github.com/passcod/slk/releases
+
+## running…
+
+### …from source:
+
+```bash
+$ $package_manager install node-webkit
+$ git clone git://github.com/passcod/slk.git
+$ cd twd
+$ nw .
+```
+
+### …from package:
+
+- Install [node-webkit].
+- Download latest [release].
+- Run it.
+
+### …on archlinux:
+
+[AUR package](https://aur.archlinux.org/packages/slk)
+
+```bash
+$ yaourt -S slk
+$ slk
+```
+
+A `.desktop` file (for graphical menus) is also provided.
+
+## legal
+
+Slack, SlackHQ, and the Slack logo are trademarks
+of, and copyrighted to, SlackHQ, Inc.
+
+All other files are released in the Public Domain as per
+my [policy](https://passcod.name/PUBLIC.txt).
---
+++ stage.js
@@ -0,0 +1,44 @@
+(function() {
+  var gui = require('nw.gui');
+  var iframe = document.querySelector('iframe');
+
+  var handleClick = function(e) {
+    var checkForLink = function(el) {
+      var openLink = function(url) {
+        gui.Shell.openExternal(url);
+        e.preventDefault();
+      };
+
+      if (el.nodeName.toLowerCase() === 'a') {
+        var href = el.getAttribute('href');
+        if (href !== null) {
+          var middleButton = e.which === 2;
+          if (middleButton || el.target === '_blank' ||
+			el.href.match(/^https:\/\/(\w+\.)?slack.com/i)) {
+            openLink(href);
+          }
+
+          console.log(href, middleButton, el.target, el.rel);
+        }
+      } else if ((p = el.parentElement) !== null) {
+        checkForLink(p);
+      }
+    };
+
+    checkForLink(e.target);
+  };
+  
+  var init = function() {
+    if (iframe &&
+        iframe.contentWindow &&
+        iframe.contentWindow.document &&
+        iframe.contentWindow.document.body &&
+        iframe.contentWindow.document.body.innerHTML) {
+     iframe.contentWindow.document.body.addEventListener('click', handleClick, false);
+    } else {
+      setTimeout(init, 100);
+    }
+  };
+  
+  init();
+}).apply(this);
